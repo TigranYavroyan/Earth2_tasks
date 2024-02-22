7cfd34415668e5ff855476f99bdc6e3dccb1a2fc
Tigran <138207789+TigranYavroyan@users.noreply.github.com>
2024-02-22 19:21:31 +0400

    Delete lev1/task17 directory

--- lev1/task17/code.js
+++
@@ -1,7 +0,0 @@
-function to_submit () {
-    var inputValue = document.getElementById("input");
-
-    if (inputValue.value.trim() === "") {
-        alert("Please enter an email address");
-    }
-}
